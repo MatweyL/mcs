e88739fcb9585d01a02893d5bc6a81148085b799
skqwk <semen.esaev@yandex.ru>
2023-11-26 17:47:35 +0300

    mcs-ui - fix extra code

--- mcs-ui/src/store/reducer/action/processor/impl/edit_action_processor.js
+++ mcs-ui/src/store/reducer/action/processor/impl/edit_action_processor.js
@@ -3,7 +3,6 @@ import {ActionProcessor} from "../action_processor";
 
 export class EditActionProcessor extends ActionProcessor {
     process(state, action) {
-        console.log(action?.payload);
         const attribute = state.attributes[state.selectedAttribute];
         attribute.value = !attribute.value;
         return {...state, attributes: {...state.attributes, [attribute.name]: attribute}};
