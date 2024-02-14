e1e4a2f55bb2332be155e89d09d56517a5bac34b
skqwk <semen.esaev@yandex.ru>
2024-02-14 22:10:52 +0300

    MCS-15 - add TextButton 'Back' on ScreenPage

--- mcs-ui/src/pages/ScreenPage.jsx
+++ mcs-ui/src/pages/ScreenPage.jsx
@@ -5,23 +5,39 @@ import Requests from "../core/constants/requests";
 import Keyboard from "../components/UI/Keyboard/Keyboard";
 import Screen from "../components/UI/Screen/Screen";
 import ScreenShadow from "../components/UI/Screen/ScreenShadow";
+import TextButton from "../components/UI/TextButton/TextButton";
+import {useNavigate} from "react-router-dom";
 
 /**
  * Страница экрана телефона
  */
 const ScreenPage = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        executeRequest(dispatch,{type: Requests.LOAD});
+        executeRequest(dispatch, {type: Requests.LOAD});
     }, []);
 
+    const back = () => {
+        navigate("/sessions");
+    }
+
     return (
-        <div className="body">
-            <div className="phone-header"/>
-            <Screen/>
-            <Keyboard/>
-            <ScreenShadow/>
+        <div style={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+            <div style={{width: "20%", justifyContent: "center", display: "flex"}}>
+                <TextButton onClick={back}>← Назад</TextButton>
+            </div>
+            <div style={{width: "50%", justifyContent: "center", display: "flex"}}>
+                <div className="body">
+                    <div className="phone-header"/>
+                    <Screen/>
+                    <Keyboard/>
+                    <ScreenShadow/>
+                </div>
+            </div>
+            <div style={{width: "20%"}}>
+            </div>
         </div>
     );
 };
