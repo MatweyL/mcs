e4bfc6620a403a1b6b1024b7648d4d6bf51fcad5
skqwk <semen.esaev@yandex.ru>
2024-02-19 10:34:52 +0300

    MCS-18 - add mappers

--- service/mapper_v2/mapper.py
+++ service/mapper_v2/mapper.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 from typing import TypeVar
 
+from service.domain_v2.channel import Channel
+from service.domain_v2.direction import Direction
 from service.domain_v2.phone import Phone
 from service.domain_v2.session import Session
 
@@ -22,18 +24,20 @@ class SessionMapper(Mapper):
     def __init__(self, phone_mapper: 'PhoneMapper'):
         self.phone_mapper = phone_mapper
 
-    def map_to_domain(self, entity: E) -> D:
-        d = D()
-        d.uid = entity['uid']
-        d.title = entity['title']
-        d.date = entity['date']
-        d.user_uid = entity['user_uid']
-        d.phone = self.phone_mapper.map_to_domain(entity['channel'])
+    def map_to_domain(self, entity: E) -> Phone:
+        phone = Phone()
 
-        return d
+        phone.uid = entity['uid']
+        phone.title = entity['title']
+        phone.date = entity['date']
+        phone.user_uid = entity['user_uid']
+        phone.phone = self.phone_mapper.map_to_domain(entity['channel'])
+
+        return phone
 
     def map_to_entity(self, session: Session) -> E:
-        entity = {}
+        entity = dict()
+
         entity['uid'] = session.uid
         entity['title'] = session.title
         entity['date'] = session.date
@@ -54,27 +58,54 @@ class PhoneMapper(Mapper):
         self.channel_mapper = channel_mapper
 
     def map_to_domain(self, entity: E) -> D:
-        d = D()
-        d.channels = self.channel_mapper.map_to_domain(entity['channels'])
-        entity['channels'] = self.channel_mapper.map_to_domain()
+        phone = Phone()
+        phone.channels = [self.channel_mapper.map_to_domain(c) for c in entity['channels']]
+        phone.directions = [self.direction_mapper.map_to_domain(d) for d in entity['directions']]
 
-    def map_to_entity(self, domain: Phone) -> E:
-        pass
+        return phone
+
+    def map_to_entity(self, phone: Phone) -> E:
+        entity = dict()
+        entity['channels'] = [self.channel_mapper.map_to_entity(c) for c in phone.channels]
+        entity['directions'] = [self.direction_mapper.map_to_entity(d) for d in phone.directions]
+
+        return entity
 
 
 class DirectionMapper(Mapper):
 
-    def map_to_domain(self, entity: E) -> D:
+    def map_to_domain(self, entity: E) -> Direction:
+        direction = Direction()
 
-        pass
+        direction.uid = entity['uid']
+        direction.name = entity['name']
 
-    def map_to_entity(self, domain: D) -> E:
-        pass
+        return direction
+
+    def map_to_entity(self, direction: Direction) -> E:
+        entity = {}
+
+        entity['uid'] = direction.uid
+        entity['name'] = direction.name
+
+        return entity
 
 
 class ChannelMapper(Mapper):
-    def map_to_domain(self, entity: E) -> D:
-        pass
+    def map_to_domain(self, entity: E) -> Channel:
+        channel = Channel()
 
-    def map_to_entity(self, domain: D) -> E:
-        pass
+        channel.uid = entity['uid']
+        channel.mode = entity['mode']
+        channel.name = entity['name']
+
+        return channel
+
+    def map_to_entity(self, channel: Channel) -> E:
+        entity = dict()
+
+        entity['uid'] = channel.uid
+        entity['mode'] = channel.mode
+        entity['name'] = channel.name
+
+        return entity
