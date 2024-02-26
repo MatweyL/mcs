3341034fd7fc00bbdede8a09327a9a6b8adcad24
MatweyL <matweyka2003@mail.ru>
2024-02-26 14:02:28 +0300

    MCS-30 added direction editor save screen processor

--- service/core/screen/processor/save/direction_editor_save_screen_processor.py
+++ service/core/screen/processor/save/direction_editor_save_screen_processor.py
@@ -2,7 +2,9 @@ from typing import Dict, Any
 
 from service.common.utils import generate_uid
 from service.core.screen.processor.save import SaveScreenProcessor
+from service.domain.channel import Channel
 from service.domain.direction import Direction
+from service.domain.phone import Phone
 from service.domain.session import Session
 
 
@@ -12,14 +14,19 @@ class DirectionEditorSaveScreenProcessor(SaveScreenProcessor):
         phone = session.phone
         if direction_id:
             direction = phone.find_direction(direction_id)
-            self._fill_direction(direction, attributes)
+            self._fill_direction(direction, attributes, phone)
         else:
             direction = Direction(uid=generate_uid())
-            self._fill_direction(direction, attributes)
+            self._fill_direction(direction, attributes, phone)
             phone.directions.append(direction)
 
-    def _fill_direction(self, direction: Direction, attributes: Dict[str, Any]):
+    def _fill_direction(self, direction: Direction, attributes: Dict[str, Any], phone: Phone):
         direction.name = attributes.get('NAME')
+        direction.channel = attributes.get('CHANNEL_ID')
+        direction.forbidden_send = attributes.get('FORBIDDEN_SEND')
+        direction.tone_call = attributes.get('TONE_CALL')
+        direction.scan_list = attributes.get('SCAN_LIST')
+        direction.economizer = attributes.get('ECONOMIZER')
 
-    def get_screen_name(self) -> str:
+    def get_screen_name(self, *args, **kwargs) -> str:
         return "DIRECTION_EDITOR"
