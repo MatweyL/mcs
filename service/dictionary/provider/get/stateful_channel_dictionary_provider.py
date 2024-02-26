fff3f5f2804298c69a008d8798615cc20a7a1099
MatweyL <matweyka2003@mail.ru>
2024-02-26 10:39:40 +0300

    MCS-23 added stateful channel dictionary provider

---
+++ service/dictionary/provider/get/stateful_channel_dictionary_provider.py
@@ -0,0 +1,17 @@
+from service.dictionary.provider.get.get_dictionary_provider import DictionaryProviderInterface
+from service.domain_v2.session import Session
+
+
+class StatefulChannelDictionaryProvider(DictionaryProviderInterface):
+
+    def provide(self, session: Session, dictionary_type: str):
+        phone = session.phone
+        dictionary_values = []
+        dictionary = dict(dictionaryName="CHANNEL",
+                          dictionaryValues=dictionary_values)
+        for channel in phone.channels:
+            dictionary_values.append(dict(name=channel.name, value=channel.uid, ))
+        return dictionary
+
+    def get_dictionary_type(self) -> str:
+        return "CHANNEL"
