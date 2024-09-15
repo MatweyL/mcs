1eb13d49fe0c91bebfb39a66d9fdd4606c256f36
skqwk <semen.esaev@yandex.ru>
2024-09-16 00:34:00 +0300

    MCS-46 - Подключение WebRTC, доработка комнат [3]

--- mcs-ui/src/config/config.js
+++ mcs-ui/src/config/config.js
@@ -1,7 +1,7 @@
 
 const urls = [
     'ws://localhost:8080',
-    'wss://localhost:8080',
+    'wss://192.168.0.102:8080',
 ]
 
-export const SOCKET_URL = urls[0];
+export const SOCKET_URL = urls[1];
